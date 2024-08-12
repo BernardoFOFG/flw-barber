60e7d87ff060d5bc17f93aa7d1b6545f507cfb74
BernardoFOFG <bernardofofg@gmail.com>
2024-08-12 00:32:21 -0300

    feat: add initial barbershop page

--- @types/index.d.ts
+++ @types/index.d.ts
@@ -3,3 +3,9 @@ import { Barbershop } from "@prisma/client"
 export declare interface BarbershopItemProps {
   barbershop: Barbershop
 }
+
+export declare interface BarbershopPageProps {
+  params: {
+    id: string
+  }
+}
